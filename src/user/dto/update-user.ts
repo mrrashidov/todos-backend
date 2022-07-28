d71a5419499c8db61452ccb44c197af909c7dc7c
usmon <usmonovahulkar1999@gmail.com>
2022-07-28 14:46:45 +0500

    fixed user

--- src/user/dto/update-user.ts
+++ src/user/dto/update-user.ts
@@ -1,9 +1,11 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { IsInt } from "class-validator";
 import { CreatedUserDto } from "./create-user.dto";
 
 export  class UpdateUserDto extends CreatedUserDto {
 
     @ApiProperty()
+    @IsInt()
     id: number;
 
 }
