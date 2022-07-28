e7b7fea449ec77975da5a5f18f201703237d70d7
usmon <usmonovahulkar1999@gmail.com>
2022-07-28 11:53:56 +0500

    crud label

--- src/label/label.module.ts
+++ src/label/label.module.ts
@@ -3,11 +3,11 @@ import { LabelService } from './label.service';
 import { LabelController } from './label.controller';
 import { Label } from './entities/label.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Color } from 'src/color/entities/color.entity';
 
 @Module({
-  imports:[TypeOrmModule.forFeature([Label])],
-
-  controllers: [LabelController],
-  providers: [LabelService]
+  imports:[TypeOrmModule.forFeature([Label,Color])],
+  providers: [LabelService],
+  controllers: [LabelController]
 })
 export class LabelModule {}
--- src/label/label.service.ts
+++ src/label/label.service.ts
@@ -1,26 +1,53 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { from } from 'rxjs';
+import { Color } from 'src/color/entities/color.entity';
+import { Repository } from 'typeorm';
 import { CreateLabelDto } from './dto/create-label.dto';
 import { UpdateLabelDto } from './dto/update-label.dto';
+import { Label } from './entities/label.entity';
 
 @Injectable()
 export class LabelService {
-  create(createLabelDto: CreateLabelDto) {
-    return 'This action adds a new label';
+  
+  constructor(
+    @InjectRepository(Label)
+    private labelRepository: Repository<Label>,
+
+    @InjectRepository(Color)
+    private colorRepository: Repository<Color>
+  ) { }
+
+  async create(createLabelDto: CreateLabelDto) {
+    const [color, label] = await Promise.all([
+      this.colorRepository.findOne({ where: { id: createLabelDto.colorId } }),
+      this.labelRepository.create({ name: createLabelDto.name })
+    ])
+    if (color != null) {
+      label.color = color;
+    }
+    return from(this.labelRepository.save(label));
   }
 
   findAll() {
-    return `This action returns all label`;
+    return from(this.labelRepository.find());
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} label`;
+    return from(this.labelRepository.findOne({ where: { id } }));
   }
 
-  update(id: number, updateLabelDto: UpdateLabelDto) {
-    return `This action updates a #${id} label`;
+  async update(id: number, updateLabelDto: UpdateLabelDto) {
+    const [label, color] = await Promise.all([
+      this.labelRepository.findOneBy({ id: id }),
+      this.colorRepository.findOne({ where: { id: updateLabelDto.colorId } })
+    ])
+    label.color = color;
+    label.name = updateLabelDto.name;
+    return from(this.labelRepository.save(label));
   }
 
   remove(id: number) {
-    return `This action removes a #${id} label`;
+    return from(this.labelRepository.delete({ id }));
   }
 }
