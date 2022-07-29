50624e92fdb15ce9bb2851cd324e38cdd64c5278
usmon <usmonovahulkar1999@gmail.com>
2022-07-29 10:39:44 +0500

    fixed color

--- src/color/dto/update-color.dto.ts
+++ src/color/dto/update-color.dto.ts
@@ -1,9 +1,3 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { IsInt } from 'class-validator';
 import { CreateColorDto } from './create-color.dto';
 
-export class UpdateColorDto extends CreateColorDto {
-    @ApiProperty()
-    @IsInt()
-    id: number;
-}
+export class UpdateColorDto extends CreateColorDto {}
