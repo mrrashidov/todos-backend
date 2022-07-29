cfaec13d7afeb5e8af549254fdd164b30f85eb2f
usmon <usmonovahulkar1999@gmail.com>
2022-07-29 15:37:25 +0500

    created new files for user

---
+++ src/user/dto/login-user.dto.ts
@@ -0,0 +1,15 @@
+
+import { ApiProperty } from '@nestjs/swagger';
+import {  IsEmail, IsNotEmpty } from 'class-validator';
+
+export class    LoginDto {
+    @ApiProperty()
+    @IsEmail()
+    email: string;
+
+    @ApiProperty()
+    @IsNotEmpty()
+    password: string;
+    
+}
+
