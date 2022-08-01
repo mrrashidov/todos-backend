cd7324f457dfa9350e169975028bebbc374bd18f
hp <mirzaqosimovafarzona@gmail.com>
2022-08-01 14:08:51 +0500

    changed for jwt

--- src/main.ts
+++ src/main.ts
@@ -3,18 +3,25 @@ import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 
-
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
-  app.useGlobalPipes(new ValidationPipe())
-
+  app.useGlobalPipes(new ValidationPipe());
 
   const swaggerConfig = new DocumentBuilder()
-    .setTitle("Todo_ist api")
-    .setDescription("Todo_ist api")
+    .setTitle('Todo_ist api')
+    .setDescription('Todo_ist api')
     .setVersion('1.0.0')
+    // .addBearerAuth(
+    //   {
+    //     description: 'Default JWT Authorization',
+    //     type: 'http',
+    //     in: 'header',
+    //     scheme: 'bearer',
+    //     bearerFormat: process.en/v.TOKEN_TYPE
+    //   },
+    //   'defaultBearerAuth',
+    // )
     .build();
 
   const document = SwaggerModule.createDocument(app, swaggerConfig);
@@ -23,4 +30,3 @@ async function bootstrap() {
   await app.listen(3000);
 }
 bootstrap();
-
